a149a6b1c1575b74a78a4b27dcbe1459907a60d1
lizandro Reyes <A18016339@alumnos.uady.mx>
2021-02-06 03:08:51 -0600

    application of good practices in coding

--- Zuul_New/src/zuul1/Game.java
+++ Zuul_New/src/zuul1/Game.java
@@ -1,84 +1,66 @@
 package zuul1;
 
 
-public class Game 
-{
+public class Game{
+    
     public static Parser parser;
     static Room startingRoom;
     GameStructure gameStructure= new GameStructure();  
     GamePrint gamePrint= new GamePrint();
 
-    public Game() {
+    public Game(){
         gameStructure.createRooms();
         parser = new Parser();
     }
     
-    
-    
-    public void startGame()
-    {
-       //GamePrint gamePrint = new GamePrint();
+    public void startGame(){
+     
        gamePrint.welcomeMessage(); 
-       boolean goOut=false; 
-       
-       while(!goOut)
-       {
-           Command cmd = parser.getUserCommand(); 
-           if(executeCommand(cmd).equals("exit")){
+       boolean goOut = false; 
+       while(!goOut){
+           Command command = parser.getUserCommand(); 
+           if( executeCommand(command).equals("exit") ){
                goOut = true;  
            }
        }  
-       String  msg="see you later and come back soon" ; 
-       System.out.println(msg);
+       String  exitMessage = "see you later and come back soon"; 
+       System.out.println(exitMessage);
     }
     
-    
-    public String executeCommand(Command cmd)
-    {
+    public String executeCommand(Command command){
      
-        CommandWord cmdWord = cmd.getaCommandWord() ; 
+        CommandWord cmdWord = command.getaCommandWord(); 
         
-        if(cmdWord== CommandWord.HELP){
+        if(cmdWord == CommandWord.HELP){
              GamePrint gamePrint = new GamePrint();
              gamePrint.helpMessage(); 
         }
-        
         if(cmdWord== CommandWord.GO){
-           gameStructure.goRoom(cmd);
+           gameStructure.goRoom(command);
         }
-        if(cmdWord== CommandWord.EXIT){
-            String msg = exit(cmd);
-            return msg;
-        }
-        
-        if(cmdWord== CommandWord.STRANGEWORD){
-             String msg = "the command is not valid"; 
-             System.out.println(msg);
-             return msg;
+        if(cmdWord == CommandWord.EXIT){
+            String exitMessage = validateExit(command);
+            return exitMessage;
         }
         
-        
-        return "";
+        if(cmdWord == CommandWord.STRANGEWORD){
+             String warningMessage = "the command is not valid"; 
+             System.out.println(warningMessage);
+             return warningMessage;
+        }
+           return "";
     }  
     
-    public String exit(Command cmd){
-        String msg="exit"; 
-        if(cmd.validateSecondaryWord()){
-            msg = "want to go out?";
-            
-        }
-    return msg;
+    public String validateExit(Command command){
+        String exitMessage = "exit"; 
+        if( command.validateSecondaryWord() ){
+            exitMessage = "want to go out?";
+          }
+    return exitMessage;
     }
-    
-    //
-    
+
     public String go(String direction) {
         return gameStructure.goRoom(new Command(CommandWord.GO, direction));        
     }
-    
-    
-    
-    
-    
-    
+
 }
